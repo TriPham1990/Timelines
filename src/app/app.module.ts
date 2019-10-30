19ae658a70576108249f72c4f04dfb6af4549525
Tri Pham <pmtri@gmail.com>
2019-10-30 10:05:06 +0700

    up bai

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {TimelinesComponent} from './timelines/timelines.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+@NgModule({
+  declarations: [
+    TimelinesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
